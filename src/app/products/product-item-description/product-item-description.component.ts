6632125704373c4383b9b8746debba366c51f043
hugocruzlfc <hugocruzlfc@gmail.com>
2024-04-22 11:56:05 +0200

    feat: adding main components

---
+++ src/app/products/product-item-description/product-item-description.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-item-description',
+  templateUrl: './product-item-description.component.html',
+})
+export class ProductItemDescriptionComponent {}
--- src/app/products/product-item-description/product-item-description.module.ts
+++ src/app/products/product-item-description/product-item-description.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductItemDescriptionComponent } from './product-item-description.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductItemDescriptionComponent
+  ],
   imports: [
     CommonModule
   ]
