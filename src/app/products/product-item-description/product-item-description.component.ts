8ee10e8fc0f4fc8e917dba180a1dbef38e209b13
hugocruzlfc <hugocruzlfc@gmail.com>
2024-04-22 13:25:54 +0200

    chore: adding reviews section

--- src/app/products/product-item-description/product-item-description.component.ts
+++ src/app/products/product-item-description/product-item-description.component.ts
@@ -9,15 +9,42 @@ import { Location } from '@angular/common';
 })
 export class ProductItemDescriptionComponent {
   currentProduct: Product = {} as Product;
+  ratings: number = 0;
+  votes: number = 0;
 
   constructor(private router: Router, private location: Location) {
     const navigation = this.router.getCurrentNavigation();
 
     this.currentProduct =
       navigation?.extras?.state!['product'] || ({} as Product);
+
+    if (typeof this.currentProduct.Reviews === 'object') {
+      this.ratings = this.currentProduct.Reviews.rating || 0;
+      this.votes = this.currentProduct.Reviews.votes || 0;
+    }
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  isObject(value: any): boolean {
+    return typeof value === 'object' && value !== null;
+  }
+
+  calculateStars(): number[] {
+    if (
+      typeof this.currentProduct.Reviews === 'string' ||
+      !this.currentProduct.Reviews.rating
+    ) {
+      return [];
+    }
+    const rating = this.currentProduct.Reviews.rating;
+    const numFullStars = Math.floor(rating!);
+    const numHalfStar = rating % 1 === 0.5 ? 1 : 0;
+    const stars = Array(numFullStars)
+      .fill(1)
+      .concat(Array(numHalfStar).fill(0.5));
+    return stars;
   }
 }
